4edb3963d5e3095ce5f16de2a0c2649a02488b8c
GabriellzinAbreu <gabriel.abreu.silva@escola.pr.gov.br>
2026-06-16 20:41:07 -0300

    teste

---
+++ bolao-backend/src/test/java/com/alfa/bolao/BolaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfa.bolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
